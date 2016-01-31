46db412123c9c0c244ec42a3694e5dc37fa745ca
berlogue <carasin.berlogue@mail.ru>
2016-01-31 10:27:36 +0300

    Modify get_sources.sh

--- get_sources.sh
+++ get_sources.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
 DEBUG=1
+REPACK=0
 
 CHROMIUM_VER=$(rpm -q --specfile *.spec --qf "%{version}")
 if [ "$DEBUG" = 1 ]; then
-    echo "$CHROMIUM_VER"
+    echo "Chromium version: $CHROMIUM_VER"
 fi
 
 rm -rf chromium-$CHROMIUM_VER
 curl -sO https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$CHROMIUM_VER.tar.xz
 tar -xf chromium-$CHROMIUM_VER.tar.xz
 
-pushd chromium-$CHROMIUM_VER/native_client &> /dev/null || :
-    if [ -d toolchain ]; then
-        if [ "$DEBUG" = 1 ]; then
-            echo "native_client/toolchain"
-        fi
-        rm -rf toolchain
+if [ -d chromium-$CHROMIUM_VER/native_client/toolchain ]; then
+    if [ "$DEBUG" = 1 ]; then
+        echo "Removing native_client/toolchain..."
     fi
-popd &> /dev/null || :
+    rm -rf toolchain
+    REPACK=1
+fi
 
-tar cavf chromium-$CHROMIUM_VER.clipped.tar.xz chromium-$CHROMIUM_VER &> /dev/null || :
+if [ "$REPACK" = 1 ]; then
+    if [ "$DEBUG" = 1 ]; then
+        echo "Repacking Chromium source..."
+    fi
+    tar cavf chromium-$CHROMIUM_VER.clipped.tar.xz chromium-$CHROMIUM_VER &> /dev/null || :
+else
+    if [ "$DEBUG" = 1 ]; then
+        echo "Renaming Chromium source..."
+    fi
+    mv chromium-$CHROMIUM_VER.tar.xz chromium-$CHROMIUM_VER.clipped.tar.xz
+fi
+
 rm -rf chromium-$CHROMIUM_VER
